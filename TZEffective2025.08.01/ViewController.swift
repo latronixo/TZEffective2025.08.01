b527ced70cae301ffcc0d58ed3d480ad3f17f8d5
Valentin <pamerra@mail.ru>
2025-08-02 12:55:32 +0300

    Initial Commit

---
+++ TZEffective2025.08.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TZEffective2025.08.01
+//
+//  Created by Валентин on 02.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
